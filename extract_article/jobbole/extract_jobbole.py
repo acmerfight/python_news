f54fe3dc5d4aea67739456f1ac6ce2c8180cf6b1
Andy Yao <yaogangqiang@admaster.com.cn>
2013-03-28 19:41:52 +0800

    抓取伯乐在线文章

--- extract_article/jobbole/extract_jobbole.py
+++ extract_article/jobbole/extract_jobbole.py
@@ -1,7 +1,31 @@
+# coding=utf-8
 from pyquery import PyQuery
-from lxml import etree
 
+def to_tryurl_list():
+    """ 要尝试的抓取的url """
+    total_page_number = 50
+    for page_number in xrange(1, total_page_number):
+        url = "http://blog.jobbole.com/tag/python/page/" + str(page_number)
+        yield url
 
-url_handle = PyQuery("http://blog.jobbole.com/tag/python/") 
-article_title = url_handle('.meta-title').text()
-print article_title
+def extract_content():
+
+    HTTP404_ERROR = "HTTP Error 404: Not Found"
+    for tryurl in to_tryurl_list(): 
+        url_handle = None
+        try:
+            url_handle = PyQuery(tryurl) 
+        except Exception as e:
+            if str(e) == HTTP404_ERROR:
+                break
+
+        article_titles = url_handle('.grid-8 .meta-title')
+        for article_title in article_titles:
+            article_title = PyQuery(article_title)
+            sub_content = (article_title.text(), article_title.attr("href"))
+            yield sub_content
+
+if __name__ == "__main__":
+    for content in extract_content():
+        print content
+
