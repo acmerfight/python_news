9cfc5cc596faa94f0795b25eea27ca353a782261
Andy Yao <acmerfight@gmail.com>
2013-06-07 00:11:14 +0800

    完成爬取伯乐在线

--- extract_article/CSDN/extract_jobbole.py
+++ extract_article/CSDN/extract_jobbole.py
@@ -3,24 +3,24 @@ from pyquery import PyQuery
 
 def to_tryurl_list():
     """ 要尝试的抓取的url """
-    total_page_number = 50
+    total_page_number = 1000000 
     for page_number in xrange(1, total_page_number):
-        url = "http://blog.jobbole.com/tag/python/page/" + str(page_number)
+        url = "http://www.csdn.net/article/tag/python/" + str(page_number)
         yield url
 
 def extract_content():
 
-    HTTP404_ERROR = "HTTP Error 404: Not Found"
     for tryurl in to_tryurl_list(): 
         url_handle = PyQuery(tryurl) 
 
-        article_titles = url_handle('.grid-8 .meta-title')
+        article_titles = url_handle('.content .unit h1')
+        #print article_titles
         if not article_titles:
             break
-        print article_titles
         for article_title in article_titles:
             article_title = PyQuery(article_title)
             sub_content = (article_title.text(), article_title.attr("href"))
+            print article_title.text(), article_title.attr("[href]")
 
 if __name__ == "__main__":
     extract_content()
